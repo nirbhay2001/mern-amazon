c1130686c2fa8e41335f57e7cc7afd7262e48bcc
nirbhay2001 <nirbhayggupta2001@gmail.com>
2023-12-25 14:45:04 +0530

    Header and Footer is complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Header from "./component/layout/Header/Header"
+import { BrowserRouter as Router,Route, Routes } from 'react-router-dom';
+import webFont from "webfontloader";
+import Footer from "./component/layout/Footer/Footer";
+import Home from "./component/Home/Home";
+
+
 
 function App() {
+
+
+  React.useEffect(()=>{
+    webFont.load({
+      google:{
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
+  },[])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header/>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+        </Routes>
+      <Footer/>
+    </Router>
   );
 }
 
