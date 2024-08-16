59f11aea70a9a93b8310373d6c38665cb6c351f9
nirbhay2001 <nirbhayggupta2001@gmail.com>
2024-08-16 19:00:33 +0530

    first commit

--- frontend/src/Urls.js
+++ frontend/src/Urls.js
@@ -1,1 +1,3 @@
-export const baseUrl = `http://192.168.56.1:4000`
+// export const baseUrl = `http://192.168.56.1:4000`
+
+   export const baseUrl = `https://e-commerce-mern-stack-including.onrender.com`
